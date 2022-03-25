bed361132f9b7db1a018ee49589982d7f56554b6
Shashika Kulasekara <skulasekara96@gmail.com>
2022-03-25 23:27:33 +0530

    Update dbSetup.sql
    Inserted database structure creation code.

--- dbSetup.sql
+++ dbSetup.sql
@@ -1,9 +1,147 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  Shashika
- * Created: Mar 10, 2022
- */
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 25, 2022 at 06:56 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `librarydb`
+--
+CREATE DATABASE IF NOT EXISTS `librarydb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `librarydb`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `category` varchar(255) NOT NULL,
+  `isbn` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `language` varchar(64) NOT NULL,
+  `state` int(11) NOT NULL COMMENT '1 = available\r\n2 = borrowed'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_lending`
+--
+
+CREATE TABLE `book_lending` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `state` int(11) NOT NULL COMMENT '1 = barrowed 2 = returned'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `id_number` varchar(64) DEFAULT NULL,
+  `id_type` varchar(64) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `gender` varchar(64) NOT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `full_name` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(64) NOT NULL,
+  `nic` varchar(20) DEFAULT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `book_lending`
+--
+ALTER TABLE `book_lending`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `book_lending` (`book_id`),
+  ADD KEY `member_lending` (`member_id`);
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `book_lending`
+--
+ALTER TABLE `book_lending`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book_lending`
+--
+ALTER TABLE `book_lending`
+  ADD CONSTRAINT `book_lending` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  ADD CONSTRAINT `member_lending` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`);
+COMMIT;
